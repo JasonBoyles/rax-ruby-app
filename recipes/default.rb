fbf584dde80ba2ffb9d71ac48479b73d548dd377
Jason Boyles <jason@duncancreek.net>
2014-05-28 15:23:07 -0500

    move app deploy/config out of default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -43,38 +43,6 @@ end
 
 include_recipe "rax-ruby-app::_ruby_#{node[:rax_ruby_app][:ruby_install_type]}"
 
-application 'app' do
-  path File.join(node[:rax_ruby_app][:user_home], 'rails_app')
-  owner node[:rax_ruby_app][:user]
-  group node[:rax_ruby_app][:group]
-  repository node[:rax_ruby_app][:git_url]
-  revision node[:rax_ruby_app][:git_revision]
-end
-
-bash "bundle install in application directory" do
-  user node[:rax_ruby_app][:user]
-  group 'sudo'
-  cwd File.join(node[:rax_ruby_app][:user_home], 'rails_app', 'current')
-  code <<-EOH
-  /opt/rubies/#{node[:rax_ruby_app][:ruby_version]}/bin/bundle install
-  EOH
-end
-
-bash 'install ref & therubyracer gems' do
-  user 'root'
-  cwd '/tmp'
-  code <<-EOH
-  /opt/rubies/#{node[:rax_ruby_app][:ruby_version]}/bin/gem install ref
-  /opt/rubies/#{node[:rax_ruby_app][:ruby_version]}/bin/gem install therubyracer
-  EOH
-end
-
-bash 'initialize app database' do
-  user node[:rax_ruby_app][:user]
-  cwd File.join(node[:rax_ruby_app][:user_home], 'rails_app', 'current')
-  code <<-EOH
-  /opt/rubies/#{node[:rax_ruby_app][:ruby_version]}/bin/bundle exec rake db:migrate
-  EOH
-end
+include_recipe "rax-ruby-app::deploy_config_app"
 
 include_recipe "rax-ruby-app::_app_server_#{node[:rax_ruby_app][:app_server]}"
