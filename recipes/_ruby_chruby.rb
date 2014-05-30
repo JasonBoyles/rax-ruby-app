8ebaf84ecdc0d83499fa0f3a0034277f85073280
Jason Boyles <jason@duncancreek.net>
2014-05-30 14:02:20 -0500

    set ruby bin dir attribute

--- recipes/_ruby_chruby.rb
+++ recipes/_ruby_chruby.rb
@@ -5,10 +5,13 @@ node.set[:chruby][:default] = node[:rax_ruby_app][:ruby_version]
 
 include_recipe 'chruby::system'
 
+node.set[:rax_ruby_app][:ruby_bin_dir] = File.join(
+  'opt', 'rubies', node[:rax_ruby_app][:ruby_version]}, 'bin')
+
 bash 'install bundler' do
   user 'root'
   cwd '/tmp'
   code <<-EOH
-  sudo /opt/rubies/#{node[:rax_ruby_app][:ruby_version]}/bin/gem install bundler
+  sudo #{File.join(node[:rax_ruby_app][:ruby_bin_dir], 'gem')} install bundler
   EOH
 end
