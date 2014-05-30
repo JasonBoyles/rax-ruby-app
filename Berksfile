561a7a17069007bacd3a149e5779861be32e419e
Jason Boyles <jason@duncancreek.net>
2014-05-30 13:50:13 -0500

    configure unicorn and init scripts

--- Berksfile
+++ Berksfile
@@ -8,5 +8,8 @@ cookbook 'mysql'
 cookbook 'application'
 cookbook 'application_ruby'
 cookbook 'database'
+cookbook 'unicorn-ng',
+  :git => 'https://github.com/chr4-cookbooks/unicorn-ng.git',
+  :ref => 'a50ee5c1d90bb4467758a3b262dd24a5dca3b013'
 
 metadata
--- recipes/_app_server_unicorn.rb
+++ recipes/_app_server_unicorn.rb
@@ -8,6 +8,8 @@ bash 'install unicorn gem' do
   EOH
 end
 
+rails_app_dir = File.join(node[:rax_ruby_app][:user_home], 'rails_app', 'current')
+
 template File.join(node[:rax_ruby_app][:user_home], 'rails_app', 'current',
                    'config', 'unicorn.rb') do
   source 'unicorn.rb.erb'
@@ -28,4 +30,55 @@ template File.join(node[:rax_ruby_app][:user_home], 'rails_app', 'current',
     stdout: File.join(node[:rax_ruby_app][:user_home], 'rails_app', 'current',
                       'unicorn_stdout.log')
   )
+end
+
+node.set['unicorn-ng']['config']['config_file'] = File.join(
+    node[:rax_ruby_app][:user_home], 'rails_app', 'current', 'config',
+    'unicorn.rb')
+node.set['unicorn-ng']['service']['rails_root'] = File.join(
+    node[:rax_ruby_app][:user_home], 'rails_app', 'current')
+node.set['unicorn-ng']['config']['worker_processes'] = 10
+node.set['unicorn-ng']['service']['user'] = node[:rax_ruby_app][:user]
+node.set['unicorn-ng']['service']['environment'] = node[:rax_ruby_app][:rails][:environment]
+node.set['unicorn-ng']['config']['listen'] = 'unix:tmp/sockets/unicorn.sock'
+node.override['unicorn-ng']['service']['bundle'] = '/opt/rubies/1.9.3-p392/bin/bundle'
+
+log "bundle path is #{node['unicorn-ng']['service']['bundle']}" do
+  level :info
+end
+
+directory File.join(rails_app_dir, 'tmp', 'pids') do
+  owner node[:rax_ruby_app][:user]
+  group node[:rax_ruby_app][:group]
+  recursive true
+  action :create
+end
+
+directory File.join(rails_app_dir, 'tmp', 'sockets') do
+  owner node[:rax_ruby_app][:user]
+  group node[:rax_ruby_app][:group]
+  recursive true
+  action :create
+end
+
+directory File.join(rails_app_dir, 'log') do
+  owner node[:rax_ruby_app][:user]
+  group node[:rax_ruby_app][:group]
+  recursive true
+  action :create
+end
+
+unicorn_ng_config File.join(node[:rax_ruby_app][:user_home],
+                   'rails_app', 'current',
+                   'config', 'unicorn.rb') do
+    user 'rails'
+    working_directory File.join(node[:rax_ruby_app][:user_home], 'rails_app',
+                                 'current')
+    listen  'unix:tmp/sockets/unicorn.sock'
+    backlog 1024
+end
+
+unicorn_ng_service File.join(node[:rax_ruby_app][:user_home], 'rails_app', 'current') do
+    bundle node['unicorn-ng']['service']['bundle']
+    environment node[:rax_ruby_app][:rails][:environment]
 end
